fab86a6848378e3ab968fae10daef826856d330f
KieronBrincat125 <kierongamer@gmail.com>
2022-10-10 08:49:35 +0200

    This is the first Commit

---
+++ DataAccess/Repositories/ItemsRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Repositories
+{
+    class ItemsRepository
+    {
+        public IQueryable<Item> GetItems()
+        {
+        }
+
+        public void AddItem()
+        {
+
+        }
+    }
+}
---
+++ Domain/Models/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Models
+{
+    //Domain: Will be a project referenced by all the other layers and will 
+    //        hold the data containers i.e Models
+
+    //Note: Code first Approach
+
+    //EFCore is the module that will be used to automatically shape/model the database
+
+    class Item
+    {
+	    [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int categoryId { get; set;  }
+        public double price { get; set; }
+        public string description { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
