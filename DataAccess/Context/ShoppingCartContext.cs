e551b90d60c3c686631f05db36e17870a41e3986
KieronBrincat125 <kierongamer@gmail.com>
2022-10-10 09:53:52 +0200

    Context class ready

---
+++ DataAccess/Context/ShoppingCartContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Context
+{
+
+    //Requirement: ensure that we have the right connection string set up in place.
+
+    //add-migration <name> ShoppingCartContext
+    //update-database
+
+    public class ShoppingCartContext : IdentityDbContext
+    {
+        public ShoppingCartContext(DbContextOptions<ShoppingCartContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Item> Items { get; set;}  //An abstraction of the tables therefore plural name
+        public DbSet<Category> Categories { get; set; }
+    }
+}
